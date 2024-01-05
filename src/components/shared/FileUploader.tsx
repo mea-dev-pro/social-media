705cfe0fe942e9e5c1e24ee592e4650fea71b432
Mamy <mamynantenaina2@gmail.com>
2024-01-05 15:41:53 +0300

    creation post en cours

---
+++ src/components/shared/FileUploader.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useState } from 'react'
+import { FileWithPath, useDropzone } from 'react-dropzone'
+import { Button } from '../ui/button'
+
+type FileUploaderProps = {
+    fieldChange: (FILES: File[]) => void;
+    mediaUrl: string;
+}
+
+
+export const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+    const [file, setFile] = useState<File[]>([])
+    const [fileUrl, setFileUrl] = useState('')
+
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        console.log(acceptedFiles);
+        // Do something with the files
+        setFile(acceptedFiles);
+        fieldChange(acceptedFiles);
+        setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+        // console.log(fileUrl);
+    }, [file])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: {
+            'image/*': ['.png', '.jpeg', '.jpg', '.svg']
+        }
+    })
+
+    return (
+        <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
+            <input {...getInputProps()} className='cursor-pointer' />
+            {
+                fileUrl ?
+                    (<div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+                        <img
+                            src={fileUrl}
+                            alt="image"
+                            className='file_uploader-img'
+                        />
+                    </div>
+                    ) : (
+                        <div className='file_uploader-box'>
+                            <img src="/assets/icons/file-upload.svg" alt="file-upload" />
+                            <h3 className='base-medium text-light-2 mb-2 mt-6'>Drag photo here</h3>
+                            <p className='text-light-4 small-regular mb-6'>SVG, PNG, JPG</p>
+
+                            <Button className='shad-button_dark_4'>
+                                Select from computer
+                            </Button>
+                        </div>
+                    )
+            }
+        </div>
+    )
+} 
